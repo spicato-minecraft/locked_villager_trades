3934a960df9bb96867fabb0f7787f83564074e77
spicato-spicato <coltond.dewberry@gmail.com>
2026-02-16 08:03:40 -0800

    Initial commit of the locked_villager_trades mod, including Gradle setup, build configuration, and basic mod structure. Added necessary files such as .gitattributes, .gitignore, gradlew scripts, and gradle wrapper properties. Implemented mod entry points and mixins for client and server integration, along with a sample icon and license file.

---
+++ src/client/java/locked_villager_trades/Locked_villager_tradesClient.java
@@ -0,0 +1,10 @@
+package locked_villager_trades;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Locked_villager_tradesClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
